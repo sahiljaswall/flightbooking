1669052c7d9674c127f05597e25c7eb102c6f05e
Sahil Jaswal <38565489+sahiljaswall@users.noreply.github.com>
2021-03-13 23:50:22 -0500

    Add files via upload

---
+++ Booking/script/scripts.js
@@ -0,0 +1,10 @@
+function trainone() {
+    ele = document.getElementsByClassName('return');
+    ele.style.display = 'block';
+    
+}
+function traintwo() {
+    ele = document.getElementsByClassName('return');
+    ele.style.display = 'none';
+    
+}
